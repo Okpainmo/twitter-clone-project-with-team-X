0be0dd54feac134a0d72ebdfbdc06b73b6b97360
Daslaw <daslaw26@gmail.com>
2023-10-11 23:30:38 +0100

    add Tailwind to the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,26 +8,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-full h-screen text-center text-4xl justify-items-center text-blue-700 flex items-center'>
+        <main>
+          <h1>Twitter Project</h1>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
